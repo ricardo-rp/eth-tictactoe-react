212526f887090339febea84cc366e9ebfbb20acc
moro <ricardoromero4444@gmail.com>
2021-09-26 15:12:56 -0300

    ✨ : Added ethers provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css';
 import Board from './components/Board'
+import { Web3Provider } from '@ethersproject/providers'
+
+import { Web3ReactProvider } from '@web3-react/core'
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <header className="App-header">
           Eth tic-tac-toe
-      </header>
-      <Board />
-    </div>
+        </header>
+        <Board />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
