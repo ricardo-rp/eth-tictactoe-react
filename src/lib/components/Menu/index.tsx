02b3253284119a6fd5603f415a00621cb0b8afd9
moro <ricardoromero4444@gmail.com>
2021-09-26 18:05:54 -0300

    ✨ : Added placeholder playerAsig

--- src/lib/components/Menu/index.tsx
+++ src/lib/components/Menu/index.tsx
@@ -11,12 +11,24 @@ const Menu: React.FC<MenuProps> = ({ hasGame, setHasGame }) => {
 
     const onClick = () => setHasGame(!hasGame)
 
+    const signature = 'asdf'
+
     return (
         <div>
             <h5 className="formLabel">New Game</h5>
             <form className="form">
-                <label htmlFor="playerASig">Player A Signature</label>
-                <input type="text" id="playerASig" />
+                <label>Player A Signature</label>
+                <input value={signature} style={{ display: 'none' }} />
+                <span
+                    style={{
+                        fontSize: 16,
+                        color: 'lightblue',
+                        border: '1px solid lightblue',
+                        borderRadius: '2px'
+                    }}
+                >
+                    {signature}
+                </span>
 
                 <label htmlFor="playerBSig">Player B Signature</label>
                 <input type="text" id="playerBSig" />
