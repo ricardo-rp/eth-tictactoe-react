a83a8b3a04a97ba96a1a942ed1d6479828729dac
moro <ricardoromero4444@gmail.com>
2021-10-06 12:49:37 -0300

    🎨 Removed redundant div

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { createGame, signGame } from './lib/utils/createGame';
 import { injected } from './components/connector';
 import { useWeb3React } from '@web3-react/core';
 
-const board = false;
-
 function Game() {
   const { active, account, chainId, activate, deactivate } = useWeb3React();
   const bindAddrB = useInput('')
@@ -58,36 +56,34 @@ function Game() {
       <button type="button" onClick={disconnect}>Disconnect</button>
 
       {/* Menu */}
-      {!board && <div>
-        <h5 className="formLabel">New Game</h5>
-        <form className="form" id="addressForm">
-          <label>Player A Address</label>
-          <label>{active ? account : "Not Connected"}</label>
-          <input id="addressA" value={account || ''} style={{ display: 'none' }} />  {/* Should auto-complete */}
+      <h5 className="formLabel">New Game</h5>
+      <form className="form" id="addressForm">
+        <label>Player A Address</label>
+        <label>{active ? account : "Not Connected"}</label>
+        <input id="addressA" value={account || ''} style={{ display: 'none' }} />  {/* Should auto-complete */}
 
-          <label htmlFor="addressB">Player B Address</label>
-          <input id="addressB" {...bindAddrB} />
+        <label htmlFor="addressB">Player B Address</label>
+        <input id="addressB" {...bindAddrB} />
 
-          <label htmlFor="nonce">Nonce</label>
-          <input id="nonce" {...nonce} />
+        <label htmlFor="nonce">Nonce</label>
+        <input id="nonce" {...nonce} />
 
-          <label htmlFor="sigA">Player A Signature</label>
-          <input id="sigA" {...sigA} />
+        <label htmlFor="sigA">Player A Signature</label>
+        <input id="sigA" {...sigA} />
 
-          <label htmlFor="sigB">Player B Signature</label>
-          <input id="sigB" {...sigB} />
+        <label htmlFor="sigB">Player B Signature</label>
+        <input id="sigB" {...sigB} />
 
-          <button type="button" onClick={onClickCreateGame}>Submit</button>
-        </form>
+        <button type="button" onClick={onClickCreateGame}>Submit</button>
+      </form>
 
-        <h5 className="formLabel">Connect by GameId</h5>
-        <form className="form">
-          <label htmlFor="gameId">Game Id </label>
-          <input type="text" id="gameId" />
+      <h5 className="formLabel">Connect by GameId</h5>
+      <form className="form">
+        <label htmlFor="gameId">Game Id </label>
+        <input type="text" id="gameId" />
 
-          <button type="button" >Submit</button>
-        </form>
-      </div>}
+        <button type="button" >Submit</button>
+      </form>
 
       <Board />
     </>
